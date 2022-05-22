7921eb803e3f2bfe1b0e22e1d86a0ddcc69c592f
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2022-05-22 18:43:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let registerEarly = true
+
+let runnersAge = 16;
+
+if (registerEarly && runnersAge > 18){
+  raceNumber += 1000;
+}
+
+if (registerEarly && runnersAge > 18){
+  console.log( `Number ${raceNumber}, you will race at 9:30 am.`);
+}
+
+else if (!registerEarly && runnersAge > 18){
+  console.log( `Number ${raceNumber}, you will race at 11:00 am.`);
+}
+
+else if (runnersAge < 18){
+  console.log(`Number ${raceNumber}, you will race at 12:30 pm`);
+}
+
+else{
+  console.log('see registration');
+}
